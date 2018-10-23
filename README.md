af3165240eb6329c8f964f3abb5468dc5ae545f1
damienobrist <damien@veezoo.com>
2018-10-23 08:43:25 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # jena-transaction-exception
+
+### Sample data
+
+The sample dataset contains a named graph `http://www.test.com/numbers` with 1'000'000 triples:
+```
+<http://www.test.com/1> rdf:type <http://www.test.com/number>
+<http://www.test.com/2> rdf:type <http://www.test.com/number>
+...
+<http://www.test.com/1000000> rdf:type <http://www.test.com/number>
+```
+### Run
+
+```
+mvn clean compile
+mvn exec:java -Dexec.mainClass="TransactionException"
+```
---
+++ src/main/java/TransactionException.java
@@ -0,0 +1,40 @@
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.system.Txn;
+import org.apache.jena.tdb2.TDB2Factory;
+import org.apache.jena.tdb2.loader.DataLoader;
+import org.apache.jena.tdb2.loader.LoaderFactory;
+import org.apache.jena.tdb2.loader.base.LoaderOps;
+
+public class TransactionException {
+
+    public static void main(String[] arguments) {
+        // load sample data from backup file into tdb
+        Dataset dataset = TDB2Factory.connectDataset("sample-data");
+        if (Txn.calculateRead(dataset, dataset::isEmpty)) {
+            DataLoader loader = LoaderFactory.createLoader(dataset.asDatasetGraph(), LoaderOps.outputToLog());
+            loader.startBulk();
+            loader.load("sample-data/sample-data.nq.gz");
+            loader.finishBulk();
+        }
+
+        // get all triples
+        String sparql = "SELECT * WHERE { ?s ?p ?o . }";
+        ResultSet results = query(dataset, sparql);
+
+        // print first solution
+        System.out.println(results.next());
+    }
+
+    private static ResultSet query(Dataset dataset, String sparql) {
+        Query query = QueryFactory.create(sparql, Syntax.syntaxARQ);
+        Model model = dataset.getNamedModel("urn:x-arq:UnionGraph");
+        return Txn.calculateRead(dataset, () -> {
+            try(QueryExecution queryExecution = QueryExecutionFactory.create(query, model)) {
+                return ResultSetFactory.copyResults(queryExecution.execSelect());
+            }
+        });
+    }
+
+}
